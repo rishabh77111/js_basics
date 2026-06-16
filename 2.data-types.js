2e249463e512d525b3faeaeb5ebbd9dc168dbe0d
Rishabh <rishabhkumaragrahari77@gmail.com>
2026-06-16 07:34:09 +0545

    add:data-types

---
+++ 2.data-types.js
@@ -0,0 +1,40 @@
+//! data types
+
+//? primitive
+
+//* number
+let a = 23;
+let b = 45.6;
+
+//* string
+let c = "hello";
+let user = "Guest";
+// let d = "";
+//? template literal
+// let e = `${2 + 2}`;
+// ``
+let e = `Hello ${user}`; //
+console.log(e); //
+
+//* boolean
+let f = true;
+let g = false;
+
+//* undefined
+let h;
+console.log(h);
+
+//* null
+let i = null;
+
+// undefined & not defined
+
+// let j;
+// console.log(j); //
+
+//? non primitive
+//* object
+//* array
+//* function
+
+//! operators
