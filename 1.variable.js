2ebf9b2f9f119a1f9952384e9c7b92d9b45a7be1
Rishabh <rishabhkumaragrahari77@gmail.com>
2026-06-16 07:30:20 +0545

    add:variable

---
+++ 1.variable.js
@@ -0,0 +1,38 @@
+// console.log("Hello world");
+
+//! variables
+// console.log(10);
+
+//! variable declaration methods
+//* let, const , var
+//var -----window mein add karta hai
+//const---use where u know there no need to change
+// int a = 12
+//  a = 'dsg'
+var x; //declare
+x = 34;//declare and intialize(pejli value dena)
+var a = 10; //
+a = "hello";
+//var a = 345;
+
+console.log(a);
+// console.log(a); //
+
+let b = 100;
+b = 45;
+
+// let b = 566;
+console.log(b);
+
+const c = 123;
+// const c = 45;
+var d;
+let e;
+
+const f = 45;
+console.log(b + a);
+let sum = b + a;
+
+console.log(sum);
+
+// c = 45;
